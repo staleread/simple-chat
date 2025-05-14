e262ab8c7a7ecee0d30289dc14f8ff7748664b80
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2025-05-14 19:03:28 +0300

    Setup template engine

---
+++ src/plugins/view.js
@@ -0,0 +1,10 @@
+import path from 'node:path'
+import view from '@fastify/view'
+import { Eta } from 'eta'
+
+export default async server => {
+  server.register(view, {
+    engine: { eta: new Eta() },
+    root: path.join(import.meta.dirname, '..', '..', 'views')
+  })
+}
